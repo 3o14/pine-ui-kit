2263239e4dbe8507d82e707a7a51e361deea2187
이원주 <3o14.dev@gmail.com>
2025-11-16 17:47:10 +0900

    chore: build setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), vanillaExtractPlugin()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "packages/components/index.ts"),
+			name: "PieUI",
+			fileName: (format) => `pie-ui.${format}.js`,
+			formats: ["es", "cjs"],
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
 });
