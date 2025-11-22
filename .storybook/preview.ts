7aa1a1789ba767fe744172899eedbaad2ff88fdd
이원주 <3o14.dev@gmail.com>
2025-11-22 22:19:27 +0900

    chore: formatting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,29 +1,32 @@
-import type { Preview } from '@storybook/react'
-import { lightTheme } from '../packages/tokens/theme.css'
-import '../packages/tokens/theme.css'
+import type { Preview } from "@storybook/react";
+import { lightTheme } from "../packages/tokens/theme.css";
+import "../packages/tokens/theme.css";
 
 // Apply light theme to storybook root
-if (typeof document !== 'undefined') {
-  document.documentElement.classList.add(lightTheme);
+if (typeof document !== "undefined") {
+	document.documentElement.classList.add(lightTheme);
 }
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-    docs: {
-      toc: true,
-    },
-    options: {
-      storySort: {
-        order: ['Components', ['Badge', 'Button', 'Checkbox', 'Dialog', 'Switch', 'TextField']],
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		docs: {
+			toc: true,
+		},
+		options: {
+			storySort: {
+				order: [
+					"Components",
+					["Badge", "Button", "Checkbox", "Dialog", "Switch", "TextField"],
+				],
+			},
+		},
+	},
 };
 
 export default preview;
