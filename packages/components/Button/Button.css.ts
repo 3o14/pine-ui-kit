d1dcd187f0960dcd2e81be4b70cccac52adef523
이원주 <3o14.dev@gmail.com>
2025-12-15 22:42:51 +0900

    chore: tokens import path

--- packages/components/Button/Button.css.ts
+++ packages/components/Button/Button.css.ts
@@ -1,6 +1,6 @@
 import { style } from "@vanilla-extract/css";
 import { recipe } from "@vanilla-extract/recipes";
-import { themeContract } from "@/tokens/theme.css";
+import { themeContract } from "@/tokens";
 
 // Base button style
 const buttonBase = style({
